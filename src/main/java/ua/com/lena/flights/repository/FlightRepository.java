a4309cd0b03e05dff1ef76c7cc0d726746f598f4
Helen1711 <helen.mysiak@gmail.com>
2021-03-29 18:37:52 +0300

    Created method for finding flights with status Active and started more than 24 hours ago.

--- src/main/java/ua/com/lena/flights/repository/FlightRepository.java
+++ src/main/java/ua/com/lena/flights/repository/FlightRepository.java
@@ -1,6 +1,7 @@
 package ua.com.lena.flights.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.com.lena.flights.entities.Flight;
 import ua.com.lena.flights.entities.FlightStatus;
 
@@ -8,4 +9,8 @@ import java.util.List;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
     List<Flight> findByStatusAndAircompanyName(FlightStatus status, String name);
+
+    @Query(value = "select * from flight where status = 'ACTIVE' and delay_started_at < now() - interval 1 day",
+            nativeQuery = true)
+    List<Flight> findByStatusAndStartedTime();
 }
